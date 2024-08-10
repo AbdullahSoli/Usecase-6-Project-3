83d20e85107af942fcf67b9010ea0ba9e263e916
Ziyad-Aljaser <120469041+Ziyad-Aljaser@users.noreply.github.com>
2024-08-10 22:10:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,39 +26,6 @@ video_html = """
 # Display the video using custom HTML with a smaller height
 components.html(video_html, height=500)
 
-st.html(
-    "<h1>السؤال السابع ؟ </h1>"
-    "<p>Scatter plot example description</p>"
-)
-
-# Example scatter plot: relationship between property age and price
-scatter_plot = alt.Chart(df).mark_circle(size=60).encode(
-    x=alt.X('propertyAge:Q', title='Property Age'),
-    y=alt.Y('price:Q', title='Price'),
-    color='location:N',
-    tooltip=['location:N', 'propertyAge:Q', 'price:Q']
-).properties(
-    title='Scatter Plot of Property Age vs. Price',
-    width=800,
-    height=400
-)
-
-st.altair_chart(scatter_plot, use_container_width=True)
-
-# Example scatter plot: relationship between property age and price
-scatter_plot1 = alt.Chart(df).mark_circle(size=60).encode(
-    x=alt.X('propertyAge:Q', title='Property Age'),
-    y=alt.Y('price:Q', title='Price'),
-    color='location:N',
-    tooltip=['location:N', 'z_score_price:Q', 'price:Q']
-).properties(
-    title='Scatter Plot of Property Age vs. Price',
-    width=800,
-    height=400
-)
-
-st.altair_chart(scatter_plot1, use_container_width=True)
-
 
 st.html(
     "<h1>السؤال الاول ؟ </h1>"
@@ -254,4 +221,21 @@ bar_chart = alt.Chart(top_10_locations).mark_bar().encode(
 st.altair_chart(donut_chart, use_container_width=True)
 st.altair_chart(bar_chart, use_container_width=True)
 
+st.html(
+    "<h1>السؤال السابع ؟ </h1>"
+    "<p>Scatter plot example description</p>"
+)
 
+# Example scatter plot: relationship between property age and price
+scatter_plot = alt.Chart(df).mark_circle(size=60).encode(
+    x=alt.X('propertyAge:Q', title='Property Age'),
+    y=alt.Y('price:Q', title='Price'),
+    color='location:N',
+    tooltip=['location:N', 'propertyAge:Q', 'price:Q']
+).properties(
+    title='Scatter Plot of Property Age vs. Price',
+    width=800,
+    height=400
+)
+
+st.altair_chart(scatter_plot, use_container_width=True)
