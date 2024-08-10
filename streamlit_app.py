4e9e9af176f248bdea06e0d6c20a283b8c576dd7
AbdullahSoli <113032879+AbdullahSoli@users.noreply.github.com>
2024-08-10 23:05:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,20 +125,7 @@ st.html(
     "<h1>السؤال الرابع ؟ </h1>"
     "<p>شرح</p>"
 )
-duplex_counts = df['duplex'].value_counts().reset_index()
-duplex_counts.columns = ['duplex', 'count']
-pie_chart = alt.Chart(duplex_counts).mark_arc().encode(
-    theta=alt.Theta(field='count', type='quantitative', title='Count'),
-    color=alt.Color(field='duplex', type='nominal', title='Duplex'),
-    tooltip=['duplex:N', 'count:Q']  
-).properties(
-    title='Distribution of Duplex Values',
-    width=400,
-    height=400
-).configure_arc(
-    outerRadius=150
-)
-st.altair_chart(pie_chart, use_container_width=True)
+
 duplex_counts = df['duplex'].map({0: 'No', 1: 'Yes'}).value_counts().reset_index()
 duplex_counts.columns = ['duplex', 'count']
 pie_chart = alt.Chart(duplex_counts).mark_arc().encode(
